2ded28431eccbe1f9aef6ea422df15b29e8e9b0d
Pradyut Nair <pradyut.nair@student.uva.nl>
2025-05-13 09:09:47 +0200

    Update directory.job, alter remove_imagenet_csv

--- spai/tools/remove_imagenet_csv.py
+++ spai/tools/remove_imagenet_csv.py
@@ -7,8 +7,8 @@ dataset_path = "/home/pnair/spai/datasets/"
 files = os.listdir(dataset_path)
 
 # Get all the files in the dataset path that are csv files and do not contain "ldm"
-csv_files = [file for file in files if file.endswith(".csv") and "ldm" not in file]
-
+#csv_files = [file for file in files if file.endswith(".csv") and "ldm" not in file]
+csv_files = ["test_set_sd3_fixed.csv"]
 # Remove the lines containing "imagenet" in the csv files
 for file in tqdm(csv_files, desc="Processing files"):
     df = pd.read_csv(os.path.join(dataset_path, file))
